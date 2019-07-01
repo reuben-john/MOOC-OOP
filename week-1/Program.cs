4555e8366ce9b59926b713326f109248a10db45e
Reuben <reuben@koppenheffer.me>
2019-07-01 07:10:30 -0500

    Complete up to part 5

--- week-1/Program.cs
+++ week-1/Program.cs
@@ -6,7 +6,50 @@ namespace week_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Print line to console
+            // Console.WriteLine("Hello World!");
+            // int months = 12;
+            // String text = "includes text";
+            // String name = "Reuben";
+            // int wholeNumber = 123;
+            // double decimalNumber = 3.141;
+            // Boolean isTrue = true;
+            // Console.WriteLine(months);
+            // Console.WriteLine(text);
+            // Console.WriteLine(wholeNumber);
+            // Console.WriteLine(decimalNumber);
+            // Console.WriteLine(isTrue);
+            // Console.WriteLine($"Hello {name}"); // String interpolation ala JS
+
+            // Types are immutable - results in error
+            // String text = "hahahaha";
+            // text = 42;
+
+            // Exercise 4:
+
+            // int chickens = 3;
+            // double bacon = 5.5;
+            // String tractor = "none";
+
+            // Console.WriteLine("Chickens:");
+            // Console.WriteLine(chickens);
+            // Console.WriteLine("bacon (kg):");
+            // Console.WriteLine(bacon);
+            // Console.WriteLine("A tractor:");
+            // Console.WriteLine($"There is {tractor}!");
+
+            // chickens = 9000;
+            // bacon = 0.1;
+            // tractor = "Zetor";
+
+            // Console.WriteLine("Chickens:");
+            // Console.WriteLine(chickens);
+            // Console.WriteLine("bacon (kg):");
+            // Console.WriteLine(bacon);
+            // Console.WriteLine("A tractor:");
+            // Console.WriteLine($"There is {tractor}!");
+
+
         }
     }
 }
