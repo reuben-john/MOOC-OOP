62929d9cf0017dd1b142b3d16835d838f8242d89
Reuben <reuben@koppenheffer.me>
2019-07-18 07:29:30 -0500

    Complete 10 and 11

--- week-2/Program.cs
+++ week-2/Program.cs
@@ -6,7 +6,40 @@ namespace week_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // variables change same way
+            // int a = 12;
+            // a += 2;
+            // a++;
+
+
+            // while loops the same
+            // Console.Write("Number: ");
+            // int a = int.Parse(Console.ReadLine());
+            // Console.Write("Number: ");
+            // int b = int.Parse(Console.ReadLine());
+            // while (a <= b)
+            // {
+            //     Console.WriteLine(a);
+            //     a++;
+            // }
+
+            //     Console.Write("Pick a number to sum up to: ");
+            //     int sum = sumSet(int.Parse(Console.ReadLine()));
+            //     Console.WriteLine($"Total: {sum}");
+
         }
+
+        // static int sumSet(int max, int min = 1)
+        // {
+        //     int total = 0;
+        //     while (min <= max)
+        //     {
+        //         total += min;
+        //         min++;
+        //     }
+
+        //     return total;
+        // }
     }
 }
